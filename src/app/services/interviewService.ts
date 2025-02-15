74c33f17d7b59013a66d8793c2cb093d3d142e87
manepallideepika25 <99087570+manepallideepika25@users.noreply.github.com>
2025-02-15 19:10:26 +0530

    api url updated

--- src/app/services/interviewService.ts
+++ src/app/services/interviewService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/interviews";
+const API_URL = "https://smartsched-api-code.onrender.com/api/interviews";
 
 export interface Interview {
   _id?: string;
